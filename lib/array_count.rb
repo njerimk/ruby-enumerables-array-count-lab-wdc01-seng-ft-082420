3ecc874517876075295a507a2a01b989a38e129f
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-15 13:56:27 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -4,7 +4,7 @@ array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
 array_2 = ["", 4, "", "goodbye",""]
 string =""
 array.count do |element|
-  puts element == string
+  puts string > 0.to_s
   end
 end
  
