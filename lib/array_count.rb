b9af7de336651671e8b719baf0463b1f62db0e54
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-15 13:55:11 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -2,9 +2,9 @@ def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable]
 array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
 array_2 = ["", 4, "", "goodbye",""]
-
-array.count do |str|
-  puts obj → str
+string =""
+array.count do |string|
+  puts string
   end
 end
  
