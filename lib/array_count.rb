9c074d2aa29aa30da453cd5d6748517b5d346b69
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-15 13:40:16 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,8 +3,8 @@ def count_strings(array)
 array_1 = ["", "hello", "world", 4, 1, 5, [], {}]
 array_2 = ["", 4, "", "goodbye",""]
 
-array.count do |string.class|
-  string.class == ""
+array.count do |string|
+  string == "".class
   end
 end
  
